1ce8ca0fd3bf7b94d96d1f55236691cf5d732429
Muhammad Aliyan Sohaib <108171361+aliyanissohaib@users.noreply.github.com>
2024-03-26 21:18:21 +0500

    Window Updation

--- main.py
+++ main.py
@@ -41,6 +41,16 @@ label.pack()
 canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
 canvas.pack()
 
+window.update()
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+
+x = (screen_width/2) - (window_width/2)
+y = (screen_height/2) - (window_height/2)
 
 
 
