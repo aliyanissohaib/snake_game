b77c7882b1cf99b11632d7b176d3293a72b2a5e7
Muhammad Aliyan Sohaib <108171361+aliyanissohaib@users.noreply.github.com>
2024-03-26 21:34:07 +0500

    Food class  completed

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 from tkinter import *
 import random
 
-GAME_WIDTH = 700
-GAME_HEIGHT = 700
+GAME_WIDTH = 600
+GAME_HEIGHT = 600
 SPEED = 50
 SPACE_SIZE = 50
 BODY_PARTS = 3
 SNAKE_COLOR = "#00FF00"
 FOOD_COLOR = "#FF0000"
-BACKGROUND_COLOR = "#00000"
+BACKGROUND_COLOR = "#000000"
 
 
 class Snake:
-    pass
+    def __init__(self):
+        
 
 class Food:
-    pass
+    def __init__(self):
+
+        x = random.randint(0, (GAME_WIDTH / SPACE_SIZE)-1) * SPACE_SIZE
+        y = random.randint(0, (GAME_HEIGHT / SPACE_SIZE)-1) * SPACE_SIZE
+
+        self.coordinates = [x,y]
+
+        canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tags="food")
+
 
 def next_turn():
     pass
@@ -54,6 +63,12 @@ y = int((screen_height/2) - (window_height/2))
 
 
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+snake = Snake()
+food = Food()
+
+
+
 
 
 window.mainloop()
